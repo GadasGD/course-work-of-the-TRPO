a2db7e4a119cc63621cde063e74c5d4d21815a90
Липина Юлия Алексеевна <is341s15@csc.sibsutis.ru>
2024-05-17 00:13:41 +0700

    Create console arg: help

--- src/main.c
+++ src/main.c
@@ -1,37 +1,45 @@
 #include "functions.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    int choice;
-    do
+    if (argc == 1)
     {
-        print_list();
-        print_menu();
-
-        choice = get_variant(6);
-        switch (choice)
+        int choice;
+        do
         {
-        case 1:
-            create_task();
-            break;
-        case 2:
-            print_task();
-            break;
-        case 3:
-            complete_task();
-            break;
-        case 4:
-            delete_task();
-            break;
-        case 5:
-            print_complete_tasklist();
-            break;
-        case 6:
-            exit(0);
-        // default:
-        //     printf("Неверный выбор операции. Попробуйте снова.\n\n");
-        }
-    } while (choice != 6);
+            print_list();
+            print_menu();
 
-    return 0;
-}
+            choice = get_variant(6);
+            switch (choice)
+            {
+            case 1:
+                create_task();
+                break;
+            case 2:
+                print_task();
+                break;
+            case 3:
+                complete_task();
+                break;
+            case 4:
+                delete_task();
+                break;
+            case 5:
+                print_complete_tasklist();
+                break;
+            case 6:
+                exit(0);
+                // default:
+                //     printf("Неверный выбор операции. Попробуйте снова.\n\n");
+            }
+        } while (choice != 6);
+
+        return 0;
+    }
+
+    if ((strcmp(argv[1], "--help")) == 0 || (strcmp(argv[1], "-h")) == 0)
+    {
+        printf("Приложение для записи заметок.\nКонсольные аргументы:\n--help или -h для вывода информации\n--create или -c для создания заметки\n--delete или -d для удаления заметки\n--complete или -cmp для отметки задачи\n--task или -t для вывода заметки\n");
+        return 0;
+    }
